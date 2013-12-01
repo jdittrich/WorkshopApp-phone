c1fba63f452516d31aa4601a81b69f3e7349f9f6
JAN DITTRICH <d_jan@ymail.com>
2013-12-01 20:15:03 +0100

    switch to jquery.countdown plugin instead of flipclock

--- workshopApp/www/js/jansCode.js
+++ workshopApp/www/js/jansCode.js
@@ -3,35 +3,31 @@ var createTimer = function(selector,time,stopCallback){
 
         
 
-        timerContainer=$('<div>',{"class":"flipclock-custom-container"}); //this will contain only the flipclock object;
-        buttonContainer=$('<div/>',{"class":"flipclock-custom-button-container"});
-    
-        timer = timerContainer.FlipClock({
-            callbacks:{
-                stop:stopCallback
-            },
-            countdown: true, //otherwise it counts upwards
-            autoStart: false, //otherwise it will "tick" right after being created
-            clockFace: "MinuteCounter"
-        });
-        timer.setTime(time);
+        timerContainer=$('<div>',{"class":"clock-custom-container"}); //this will contain only the flipclock object;
+        buttonContainer=$('<div/>',{"class":"clock-custom-button-container"});
         
+        timer = $(timerContainer)
+        .countdown({until:time,
+                    onExpiry:stopCallback,
+                    compact:true,
+                    format:"MS"})
+        .countdown('pause'); //it autostart otherwise
+                    
         timerContainer.appendTo(selector);
         
         startButton = $("<button/>",{
             text:"start", 
-            "class":"flipclock-custombutton flipclock-custombutton-start"
+            "class":"clock-custombutton clock-custombutton-start"
         }).click(function(){
-            if(timer.timer.factory.time<=0){return;} //<= because when the displayed time is  on 0, the time value is actually -1 (possible we trigger an additional flip in the start function) 
-            timer.start(); //starts the timer
-            timer.flip();//due to a bug (?) the first flip occurs not after 1, but after 2 seconds, as the first triggering of the flip method changes nothing. So we trigger it here the first time. 
+            $(timer).countdown("resume");
         });
         resetButton = $("<button/>",{
             text:"reset", 
             "class":"flipclock-custombutton flipclock-custombutton-reset"
         }).click(function(){
-            timer.stop();//otherwise it will just continue after reset
-            timer.setTime(time); //sets the time to the time value passed for initialization. Closures FTW. 
+            $(timer).countdown('option', 
+    {until: time});
+            $(timer).countdown("pause");
         });
     
         $(buttonContainer).append(startButton, resetButton);
@@ -60,11 +56,7 @@ $(document).on("pagebeforecreate",function(event){
             throw "data-clock-duration attribute is not a number (NaN): "+typeof duration;
         }else{
             createTimer(element,duration, function(){
-                if(this.factory.time.time!==0){return}// since the function will be called everytime the timer is stopped, not only when the time is up (on 0). So every stop done while there is still time left, leads to an return. 
-                //TODO: needs some check that the timer is finished and not just reset. 
-                console.log("callback?!");
-                //this function will be called when the timer reached 00:00 
-                
+               
                 if(navigator && navigator.notification && navigator.notification.beep){//if on phonegap
                     navigator.notification.beep(1);        
                 }else{ //else use the normal browser sound
