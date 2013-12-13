8a7f1eaf56c667c6bc50758f13c0470d433cf434
jan <d_jan@ymail>
2013-12-13 19:11:52 +0100

    css, structure with image popups, javascript with clock transitions and prompt text for users

--- workshopApp/www/js/jansCode.js
+++ workshopApp/www/js/jansCode.js
@@ -1,23 +1,65 @@
-var createTimer = function(selector,time,stopCallback){
-        var timerContainer, timer,startButton, resetButton;
+var reset
+
+
+var createTimer = function(selector,time){
+        var thisContainer,timerContainer, timer,startButton, resetButton, buttonContainer, timerEnd, timerStart, timerReset, notificationContainer;
         var config={
+            timerNotificationText:"Next person – get ready to present and start timer!",
+            timerNotificationClass:"clock-custom-notifications",
             timerContainerClass: "clock-custom-container",
             timerButtonsContainerClass:"clock-custom-button-container",
         };
         
-
-        timerContainer=$('<div>',{"class":config.timerContainerClass}); //this will contain only the flipclock object;
+        thisContainer = $(selector);
+        timerEnd = function(){
+            //BEEP part
+            if(navigator && navigator.notification && navigator.notification.beep){
+                //IF on phonegap
+                navigator.notification.beep(1);        
+            }else{ 
+                //ELSE (IF not on Phonegap) use the normal browser sound
+                var soundFile= new Audio();
+                soundFile.src="img/beep.ogg";
+                soundFile.play();
+            }//END else 
+            
+            
+            //RESET Part
+            thisContainer.fadeTo(400,0.1).fadeTo(600,1, function(){
+                notificationContainer.fadeTo(200,1);
+            });
+            
+            $(timer).countdown('option', {until: time});
+            $(timer).countdown("pause");            
+        };//END function
+        
+        //Starts the timer
+        timerStart = function(){
+            $("."+config.timerContainerClass).trigger("timerstart",{"target":timerContainer}); //with on("timerstart"…) timers can get this event.
+            $(timer).countdown("resume");
+            notificationContainer.fadeTo(200,0.0);
+        };
+        
+        //resets the timer to the orignally passed timer
+        timerReset = function(){
+            $(timer).countdown('option', {until: time});
+            $(timer).countdown("pause");
+        };
+        
+        timerContainer=$('<div>',{"class":config.timerContainerClass}); //this will contain only the clock object;
         buttonContainer=$('<div/>',{"class":config.timerButtonsContainerClass});
+        notificationContainer = $('<div/>',{
+            "class":config.timerNotificationClass,
+            "text":config.timerNotificationText
+        }).css("opacity","0.0");
         
         timer = $(timerContainer)
         .countdown({until:time, //time in seconds
-                    onExpiry:stopCallback, //the "alarm" when the time is off
+                    onExpiry:timerEnd, //the "alarm" when the time is off
                     compact:true, //no Minutes:xx Seconds:xx
                     format:"MS"})
         .countdown('pause'); //it autostarts. And this stops it. 
                     
-        timerContainer.appendTo(selector);
-        
         //react on timerstart event in order to stop if another timer is started
         timerContainer.on("timerstart", function(e){
             if(e.target != timerContainer){
@@ -25,24 +67,19 @@ var createTimer = function(selector,time,stopCallback){
             }
         });
     
-        startButton = $("<button/>",{
-            text:"start", 
-            "class":"clock-custombutton clock-custombutton-start"
-        }).click(function(){
-            $("."+config.timerContainerClass).trigger("timerstart",{"target":timerContainer});
-            $(timer).countdown("resume");
-        });
+        startButton = $("<button/>",{ //START-Button
+            text:"start",
+            "class":"clock-custombutton clock-custombuttoButtonsContainerClassn-start"
+        }).click(timerStart);
+    
         resetButton = $("<button/>",{
             text:"reset", 
-            "class":"flipclock-custombutton flipclock-custombutton-reset"
-        }).click(function(){
-            $(timer).countdown('option', 
-    {until: time});
-            $(timer).countdown("pause");
-        });
-    
+            "class":"clock-custombutton flipclock-custombutton-reset"
+        }).click(timerReset);
+        
+        //$(selector).append(timerContainer);
         $(buttonContainer).append(startButton, resetButton);
-        $(selector).append([timerContainer,buttonContainer]); //lesser known: pass $.append an array of objects to insert them at once. 
+        thisContainer.append([notificationContainer,timerContainer,buttonContainer]); //lesser known: pass $.append an array of objects to insert them at once. 
         
         return timer;
 };//createTimer End
@@ -66,17 +103,7 @@ $(document).on("pagebeforecreate",function(event){
         if(typeof duration !== "number"){
             throw "data-clock-duration attribute is not a number (NaN): "+typeof duration;
         }else{
-            createTimer(element,duration, function(){
-               
-                if(navigator && navigator.notification && navigator.notification.beep){//if on phonegap
-                    navigator.notification.beep(1);        
-                }else{ //else use the normal browser sound
-                    var soundFile= new Audio();
-                    soundFile.src="img/beep.ogg";
-                    soundFile.play();
-                }//END else 
-            }//END function
-        );//end createTimer call
+            createTimer(element,duration);//end createTimer call
         
         }//end else
     });//end each callback function and each function call   
