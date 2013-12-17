7abeedd33ac0c51e512a6db340ea459a342e1dbb
JAN DITTRICH <d_jan@ymail.com>
2013-12-17 11:37:31 +0100

    wrapped transition by order functionality into a function so we can return or throw on trouble and let the rest of the code go on

--- workshopApp/www/js/jansCode.js
+++ workshopApp/www/js/jansCode.js
@@ -27,10 +27,11 @@ var createTimer = function(selector,time){
             //RESET Part
             thisContainer.fadeTo(400,0.1).fadeTo(600,1, function(){
                 notificationContainer.fadeTo(200,1);
+                $(timer).countdown('option', {until: time});
+                $(timer).countdown("pause"); 
             });
             
-            $(timer).countdown('option', {until: time});
-            $(timer).countdown("pause");            
+                       
         };//END function
         
         //Starts the timer
@@ -172,61 +173,69 @@ $(document ).on( "pageshow",function(event, ui){
 });
     
 $(document ).on( "pagebeforechange",function(event, data){  
-    /*
-    functionality that finds the digits the id starts with.
-    it matches strings so it is rather unsave/dirty. 
-    Alternative: give pages a data-order attribute and have "43-33-32" or the like as attribute than you dont have the error prone extration operation going on.
-    */
     
-    var pageTo, pageFrom, pageToOrderNumber, pageFromOrderNumber;
-    
-    if (data.toPage instanceof jQuery){
-        pageToString = data.toPage.attr("id");
-    }else if (typeof data.toPage === "string"){
-        pageToString = data.toPage;
-    }
+    (function(event, data){
+        /*
+        functionality that finds the digits the id starts with.
+        it matches strings so it is rather unsave/dirty. 
         
-    
-    /*1. Get the substring from the last occurence of "#" to the end of the string
-      2. in this string, match all numbers followed by a "-" like 1- in "asdsa1-awde"
-      3. an array will be returned. We dont expect more than 1 match, but even if, just take the first item [0] 
-      4. replace all "-" with "" (nothing) to get the number itself */
-    if(pageToString.search(/#.*[\d-]/) !== -1){
-        pageToOrderNumber= parseFloat(pageToString.substring(pageToString.lastIndexOf("#")).match(/[\d-]+/)[0].replace(/-/g,""));
-        console.log(pageToOrderNumber);
-    }
-    
-    
-    
-    if (data.options.fromPage instanceof jQuery){ //in a single page app it should be always a jq object
-        pageFromString = data.options.fromPage.attr("id");
-    }
-    
-     if(pageToString.search(/#.*[\d-]/) !== -1){//there is some "#" and some digit 
-         
-         
+        Wrapped into a function so we can return or throw if trouble occurs
+        
+        Alternative: give pages a data-order attribute and have "43-33-32" or the like as attribute than you dont have the error prone extration operation going on.
+        */
+        var pageTo, pageToString, pageToOrderNumber,pageFrom, pageFromString,  pageFromOrderNumber;
+        
+        if (data.toPage instanceof jQuery){
+            pageToString = data.toPage.attr("id");
+        }else if (typeof data.toPage === "string"){
+            pageToString = data.toPage;
+        }else{
+            return;
+        }
+            
+        
         /*1. Get the substring from the last occurence of "#" to the end of the string
-      2. in this string, match all numbers followed by a "-" like 1- in "asdsa1-awde"
-      3. an array will be returned. We dont expect more than 1 match, but even if, just take the first item [0] 
-      4. replace all "-" with "" (nothing) to get the number itself */  
-         pageFromOrderNumber = parseFloat(pageFromString.substring(pageFromString.lastIndexOf("#")).match(/[\d-]+/)[0].replace(/-/g,""));
+          2. in this string, match all numbers followed by a "-" like 1- in "asdsa1-awde"
+          3. an array will be returned. We dont expect more than 1 match, but even if, just take the first item [0] 
+          4. replace all "-" with "" (nothing) to get the number itself */
+        if(pageToString.search(/#.*[\d-]/) !== -1){
+            pageToOrderNumber= parseFloat(pageToString.substring(pageToString.lastIndexOf("#")).match(/[\d-]+/)[0].replace(/-/g,""));
+            console.log(pageToOrderNumber);
+        }
+        
+        
+        
+        if (data.options.fromPage instanceof jQuery){ //in a single page app it should be always a jq object
+            pageFromString = data.options.fromPage.attr("id");
+        }else{
+            return;
+        }
+        
+        if(pageFromString.search(/#.*[\d-]/) !== -1){//there is some "#" and some digit 
              
-    }
-    
-    //If both numbers are there…
-    if(!isNaN(pageToOrderNumber)&& !isNaN(pageFromOrderNumber)){ //there is just a native "isNaN" so we reverse it (!) to make it an is-number-function
-        if(pageToOrderNumber>pageFromOrderNumber){
-            data.options.transition = "slide";
-            data.options.reverse = false;
+             
+            /*1. Get the substring from the last occurence of "#" to the end of the string
+          2. in this string, match all numbers followed by a "-" like 1- in "asdsa1-awde"
+          3. an array will be returned. We dont expect more than 1 match, but even if, just take the first item [0] 
+          4. replace all "-" with "" (nothing) to get the number itself */  
+             pageFromOrderNumber = parseFloat(pageFromString.substring(pageFromString.lastIndexOf("#")).match(/[\d-]+/)[0].replace(/-/g,""));
+                 
         }
-        if(pageToOrderNumber<pageFromOrderNumber){
-            data.options.transition = "slide";
-            data.options.reverse = true;
+        
+        //If both numbers are there…
+        if(!isNaN(pageToOrderNumber)&& !isNaN(pageFromOrderNumber)){ //there is just a native "isNaN" so we reverse it (!) to make it an is-number-function
+            if(pageToOrderNumber>pageFromOrderNumber){
+                data.options.transition = "slide";
+                data.options.reverse = false;
+            }
+            if(pageToOrderNumber<pageFromOrderNumber){
+                data.options.transition = "slide";
+                data.options.reverse = true;
+            }
         }
-    }
-    
-    console.log(pageFromOrderNumber, pageToOrderNumber);
-    
+        
+        console.log(pageFromOrderNumber, pageToOrderNumber);
+    }(event,data))
  /*
  seemingly, one can change the 
  data.options.transition="slide"
